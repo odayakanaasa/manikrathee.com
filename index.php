903ea43b3f97c1a5395b32bf0c83c6b66e95095e
Manik Rathee <mrathee@barackobama.com>
2012-02-13 02:26:05 -0600

    Initial Commit - Pre server sync

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php $section='home'; ?>
+<?php include('global/header.php'); ?>
+
+<body class="home">
+	<div id="home_container">
+		<section class="main">
+			<section class="title">	
+				<h1 id="home_title">Hi, I'm Manik.</h1>
+					i’m a front end developer for <a href="http://www.barackobama.com/" target="_blank">Obama For America</a> based out of lovely chicago, il. I am also a <a href="http://www.manikratheephotography.com" target="_blank">wedding &amp; event photographer</a>. Sometimes <a href="http://blog.manikrathee.com/" target="_blank">I write about design and development</a>, and I really (really) <a href="http://www.rdio.com/#/people/manikrathee/" target="_blank">like music</a>. 
+			</section>
+			<section class="social">
+				<ul>
+					<li><a href="http://www.twitter.com/manikrathee" target="_blank"><img class="tooltip" title="Twitter" src="images/home/twitter.png"></a></li>
+					<li><a href="http://www.dribbble.com/manikrathee" target="_blank"><img class="tooltip" title="Dribbble" src="images/home/dribbble.png"></a></li>
+					<li><a href="http://manikrathee.tumblr.com" target="_blank"><img class="tooltip" title="Tumblr" src="images/home/tumblr.png"></a></li>
+					<li><a href="http://instagrid.me/manikrathee/" target="_blank"><img class="tooltip" title="Instagram" src="images/home/instagram.png"></a></li>
+				</ul>
+				<div class="clear"></div>
+			</section>
+			<section class="projects">
+				<h2>Recent Projects</h2>
+				<ul>
+					<li><a href="interior_catalyst.php">Catalyst</a> - Minimal iOS Theme</li>
+					<li><a href="interior_hope.php">Hope Will See Us Through</a> - Typographic Experiments</li>
+					<li><a href="interior_mrphoto.php">Manik Rathee Photography</a> - Wedding &amp; Event Photography</li>
+					<li><a href="interior_wrap.php">Wrapskins</a> - Web App UI Design &amp; UX</li>
+					<li><a href="interior_livescribe.php">Livescribe</a> - Digital Pen Advertising Site</li>
+					<li><a href="interior_universalshanti.php">Universal Shanti</a> - Blog and Website for Reiki</li>
+					<li><a href="interior_blog.php">Blog</a> - My Design &amp; Dev Blog</li>
+					
+				</ul>
+			</section>
+		</section>
+	</div>
+	
+	
+	
+<?php include('global/footer.php'); ?>
