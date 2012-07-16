b816f469f7e30feacb7e18db29141cc814bc539f
manikrathee <manikrathee@me.com>
2012-07-16 18:32:06 -0500

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true  # Must be set for the password prompt
+                                  # from git to work
+set :repository, "git@github.com:manikrathee/manikrathee.com.git"  # Your clone URL
+set :scm, "git"
+set :user, "manikrathee.com"  # The server's user for deploys
+set :scm_passphrase, "vectorone1"  # The deploy user's password
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-set :scm, :subversion
+#set :application, "set your application name here"
+#set :repository,  "set your repository location here"
+
+#set :scm, :subversion
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
