6fb1b31212af075b92ca3cd7fa9d06cd74c45b43
Manik Rathee <manikrathee@gmail.com>
2012-10-19 11:53:29 -0500

    added classes for symbolset

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-//@codekit-prepend "libs/jquery-1.8.2.min.js","libs/jquery.backstretch.js","libs/jquery.fittext.js","libs/jquery.flexslider.js","libs/jquery.api.instagram.js","libs/jquery.api.twitter.js","libs/jquery.twitter.js","libs/jquery.lettering.js","libs/jquery.prettyForms.js","libs/jquery.tipTip.js";
+//@codekit-prepend "libs/jquery-1.8.2.min.js","libs/jquery.backstretch.js","libs/jquery.fittext.js","libs/jquery.flexslider.js","libs/jquery.api.instagram.js","libs/jquery.api.twitter.js","libs/jquery.twitter.js","libs/jquery.prettyForms.js","libs/jquery.tipTip.js";
 
-// "libs/ss-social.js", "libs/ss-standard.js"
+// "libs/ss-social.js", "libs/ss-standard.js", "libs/jquery.lettering.js"
 
 // Nav Hover
 function checkWidth() {
