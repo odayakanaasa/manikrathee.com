c883ee4898c5cce28a9e9421cd87205e74b93ff4
Manik Rathee <manikrathee@gmail.com>
2013-03-17 21:06:24 -0700

    added contribute page

---
+++ barack-obama-contribute.php
@@ -0,0 +1,47 @@
+<?php $section='hope'; $upper='white'; $project='true'; ?>
+<?php include('global/header.php'); ?>
+<body class="interior obama-contribute">
+	<div class="upper obama-contribute-upper">
+	<?php include('global/nav.php'); ?>
+		<div class="container">
+			<div class="row">
+				<div class="sevencol"></div>
+				<div class="fivecol last">
+					<div class="obama-contribute-title">
+						<img src="images/hope_heading.png">
+						<div class="subtitle">
+							typographic experiments
+						</div>
+					</div>
+				</div>
+			</div><!-- END row -->
+		</div>
+	</div>
+	<div class="container body">
+		<div class="row section_spacer">
+			<div class="twelvecol last main-section">
+				<div class="left_col_inner">
+					<div class="article">
+						<div class="headline">
+							<h3 class="fit">Typography &amp; Music</h3>
+						</div>
+						<p>Hope Will See Us Through is a side project I started early this year. I was listening to a song, <a href="http://www.last.fm/music/Bayside/Killing+Time/It%27s+Not+A+Bad+Little+War" title="It's Not A Bad Little War by Bayside" target="_blank">Its Not A Bad Little War by Bayside</a>, for what must have been the thousandth time. At some point in the breakdown, I realized that I had listened to it over and over, but I had never actually <em>heard</em> it.</p>
+					
+						<p>It was at this moment that I chose the very next line and decided to create a typographic blog dedicated to bringing lyrics to life. And so, HOPE was born.</p>
+					</div>
+					<div class="article">	
+						<div class="headline">
+							<h2 class="fit">Process</h2>
+						</div>
+						<p>I originally intended to create a new piece every week. This plan quickly changed because I realized that just picking a line in a song and making a piece out of it wasn’t what I really wanted.</p>
+					
+						<p>I wanted to take music that meant something to me and really feel it. Take a line that could send shudders down your spine and make it immortal.</p>
+					
+						<p>This is why my updates have become erratic. I won’t update just for the sake of updating. I’ll update when a song strikes me and I will listen to that song for hours while I sketch. At some point,  lyrics work their way into the sketches and thats when they make their way onto the screen.</p>
+					</div>
+				</div>
+			</div>
+		</div><!-- END row -->
+		
+	</div><!-- END container -->	
+	<?php include('global/footer.php'); ?>
