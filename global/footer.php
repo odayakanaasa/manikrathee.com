73e52305ff82a5ddf42d60af29a6b67e2167ae1d
Manik Rathee <manikrathee@gmail.com>
2013-05-19 00:04:48 -0700

    cleaned up base styles, type and footer back to top link

--- global/footer.php
+++ global/footer.php
@@ -1,4 +1,4 @@
-	<a id="top" class="ss-icon" href="#" title="Back To The Top">Up <span>To the top</span></a>
+	<a id="top" class="ss-icon" href="#" title="Back To The Top">Up</a>
 
 	<script src="js/scripts-ck.js"></script>
 <?php if ($section=='contribute') { echo '          <script src="js/sequential-ck.js"></script>'; } ?>
