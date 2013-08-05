7d37c35996a53bb912cd8da0308fe41880942da3
Manik Rathee <manikrathee@gmail.com>
2013-08-05 09:23:03 -0700

    updates for hammer

--- is/a/tweeter/data/js/user_details.js
+++
@@ -1,8 +0,0 @@
-var user_details =  {
-  "screen_name" : "ManikRathee",
-  "location" : "Montclair, NJ",
-  "full_name" : "Manik Rathee",
-  "bio" : "Lead Designer at @Fluence_app.  \r\n\r\nPreviously, User Experience Engineer for President @BarackObama.",
-  "id" : "217990551",
-  "created_at" : "Sun Nov 21 03:16:56 +0000 2010"
-}
