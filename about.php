37f93e3fb166637dfee994dc2d965d53320a8694
Manik Rathee <manikrathee@gmail.com>
2013-08-05 10:09:47 -0700

    updated about page with speaking engagements

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php $section='about'; $upper='white'; $project='false';?>
 <?php include('global/header.php'); ?>
-	
+
 <body class="interior about">
 	<div class="upper">
 	<?php include('global/nav.php'); ?>
@@ -19,15 +19,15 @@
 				<div class="article">
 					<h3 class="fit">Always More To Learn</h3>
 					<p>I'm always looking for a new challenge. Every day something new comes out in the world of design and development and along with that comes new problems to solve. Tackling these problems helps me to grow as a thinker, as a designer, as a developer and as a creative in general.</p>
-					
+
 					<p>I've loved design, front-end development and user-focused research since I was first exposed to the web. I've had the privilege of working with companies like AT&amp;T and T-Mobile while freelancing, which helped get me the skills that landed me at <a href="http://www.revsystems.com/" title="RevSystems, Inc">RevSystems, Inc</a> as the Lead Designer. I had a lot of freedom while working at Rev, both with client work and to apply my time towards experimenting with new web tech. It was here that I was able to experiement with a lot of frameworks and, more importantly, with responsive design.</p>
-					
+
 					<p>As soon as I stumbled onto responsive design, I started applying it to all of our projects. We built responsive websites, CMS control panels and more. I spent endless nights [foolishly] <a href="https://github.com/manikrathee/1140gs-Custom" title="1140 GS on Github">trying to</a> build <a href="https://github.com/manikrathee/FutureGS" title="FutureGS on Github">CSS frameworks</a> that could handle all the problems that responsive design presented. </p>
-					
+
 					<p>Around this time, I got an e-mail from <a href="http://dryan.com/" title="Daniel Ryan">Daniel Ryan</a>, the Director of Frontend Development for President Brack Obama. I interviewed to join the frontend dev team, got the job and promptly moved out to Chicago to start one of the most amazing journies of my life. I worked under the Lead Developer, <a href="http://jefflouden.com/" title="Jeffrey Louden">Jeffrey Louden</a> for a while, improving the UX of <a href="http:/www.barackobama.com/" title="BarackObama.com">BarackObama.com</a> and then jumped over to the Contribute team under <a href="http://www.kylerush.net/" title="Kyle Rush">Kyle Rush</a>, the Deputy Director of Development. We <a href="http://blog.manikrathee.com/posts/2012/11/16/the-tech-president.html" title="built some stuff">built some pretty groundbreaking stuff</a>, broke <del>some</del> <a href="http://swampland.time.com/2012/11/15/exclusive-obamas-2012-digital-fundraising-outperformed-2008/" title="broke some records">all of the records</a>, and <a href="http://www.youtube.com/watch?v=IeCY-jKpoZ0" title="elected a president">elected a president</a>.</p>
-					
+
 					<p>And now, I'm headed out to beautiful San Francisco to join <a href="http://www.fluenceapp.com" title="Fluence App">Fluence</a> - a great new media-based startup. Forward.</p>
-					
+
 					<img src="images/potus-hug.jpg" alt="Yep, President Barack Obama giving me a hug">
 					<figcaption> For the final day of the campaign,the President of the United States thanked me for my work with a strong handshake and a hug. It was cool.</figcaption>
 				</div>
@@ -40,14 +40,14 @@
 					<li>
 						<span class="ss-icon">Sound</span>
 						<a class="published-title" href="http://2013.cssdevconf.com/" title="CSS Dev Conf">
-							"The Tech President"
+							"Building Obama, Part 2"
 							<span>CSS Dev Conf</span>
 							<span>Estes Park, CO &nbsp; &mdash; &nbsp; Oct. 21-23, 2013</span>
 						</a>
 					</li>
 					<li>
 						<span class="ss-icon">Sound</span>
-						<a class="published-title" href="http://2013.cssdevconf.com/" title="Thinking Creatively Conference 10">
+						<a class="published-title" href="http://blog.manikrathee.com/posts/2013/05/29/building-obama.html" title="Thinking Creatively Conference 10">
 							"Building Obama"
 							<span>Keynote Address, Thinking Creatively Conference</span>
 							<span>Union, NJ &nbsp; &mdash; &nbsp; Apr. 18-20, 2013</span>
@@ -57,7 +57,7 @@
 						<a href="http://lanyrd.com/profile/manikrathee/" title="ManikRathee on Lanyrd" itemprop="url"><span class="ss-icon">right</span>View more on Lanyrd</a>
 					</li>
 				</ul>
-				
+
 				<h4>Interviews &amp; Features</h4>
 				<p>I have had the privilege of being interviewed, having my worked featured and appearing on some podcasts. Here are a select few of those:</p>
 				<ul id="published-list" class="title-subtitle-list">
@@ -109,17 +109,17 @@
 			</div>
 		</div><!-- END row -->
 	</div><!-- END container -->
-	
+
 	<div class="fact-container">
 		<div class="container row">
 			<p id="quick-facts">Quick Facts</p>
 			<ul id="about-data">
 				<li id="i-take-photos">
 					<span class="ss-icon">camera</span>
-					<p>I take about 
+					<p>I take about
 						<span class="number-container">
 							<span id="number-one" class="number" data-numer="25">0</span> photos
-						</span> 
+						</span>
 						a day
 					</p>
 				</li>
@@ -128,7 +128,7 @@
 					<p>I run a
 						<span class="number-container">
 							<span id="number-two" class="number" data-number="420">0</span> second
-						</span> 
+						</span>
 						mile
 					</p>
 				</li>
@@ -137,22 +137,22 @@
 					<p>I listen to
 						<span class="number-container">
 							<span id="number-three" class="number" data-number="62">0</span>+ hours
-						</span> 
+						</span>
 						of music every week
 					</p>
 				</li>
 			</ul>
 		</div><!-- END container row -->
-	</div><!-- END fact-container -->	
-		
-	<!-- 
+	</div><!-- END fact-container -->
+
+	<!--
 	This is so not done.
-	
+
 	Like, not at all.
-	
+
 	Not even a little bit.
-	
-	
+
+
 	<div id="map">
 		<div class="container row">
 			<p><span class="ss-icon">Location</span> Current Location: <span class="location">Montclair, NJ</span></p>
@@ -172,6 +172,6 @@
 			</ul>
 		</div>
 	</div> -->
-	
-			
+
+
 	<?php include('global/footer.php'); ?>
