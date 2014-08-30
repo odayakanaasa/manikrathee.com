e1090e9ab41caede6844f2c44f8028593389cefd
Manik Rathee <manikrathee@gmail.com>
2014-08-30 11:41:23 -0700

    removed fitText

--- _prebuild/js/libs/script.js
+++ _prebuild/js/libs/script.js
@@ -137,13 +137,6 @@ toTop.click(function(e) {
 });
 
 
-//FitText
-// $(".interior h2").fitText(1.2);
-$('.interior h3').fitText(1.3);
-$('.fit').fitText(1.2);
-$('.fit-light').fitText(0.8);
-
-
 //Counter
 var countNumberValue;
 function startCount() {
