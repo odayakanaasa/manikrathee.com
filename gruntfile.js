7cc0ae80580e395faba5f35f1548cdb288f79820
Manik Rathee <manikrathee@gmail.com>
2016-08-02 23:59:50 -0700

    group add

--- gruntfile.js
+++ gruntfile.js
@@ -34,12 +34,16 @@ module.exports = function(grunt) {
        main: {
         src: [
           '_site/js/libs/analytics.js',
+          '_site/js/libs/scrolldepth.js',
           '_site/js/libs/mo.min.js',
           '_site/js/libs/jquery.withinViewport_base.js',
           '_site/js/libs/jquery.withinViewport.js',
           '_site/js/libs/jquery.api.twitter.js',
           '_site/js/libs/jquery.api.last.fm.js',
           '_site/js/libs/jquery.api.instagram.js',
+          '_site/js/libs/lazyload.js',
+          '_site/js/libs/konami.js',
+          '_site/js/libs/picturefill.js',
           '_site/js/libs/jquery.twitter.js',
           '_site/js/sequential.js',
           '_site/js/libs/script.js',
