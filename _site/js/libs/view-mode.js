3333b5f6b4ad0cf68437f62e8f03425bae2c97a0
Manik Rathee <manikrathee@gmail.com>
2017-03-14 17:50:46 -0700

    prod build

--- _site/js/libs/view-mode.js
+++ _site/js/libs/view-mode.js
@@ -1,41 +1,39 @@
+;(function () {
 // View Mode Toggle
 
-var viewModeContainer = $('#view-mode--container');
-var viewModeToggle    = $('#view-mode--toggle');
+  var viewModeContainer = document.getElementById('view-mode--container');
+  var viewModeToggle    = document.getElementById('view-mode--toggle');
+  
 
-var viewMode = {
+  var viewMode = {
+    // TODO set & eval a cookie to remember this value
 
-  // TODO set & eval a cookie to remember this value
+    init: function() {
+      this.addEventListeners();
+    },
 
-  init: function() {
-    this.addEventListeners();
-  },
+    addEventListeners: function() {
+      var _this = this;
 
-  addEventListeners: function() {
-    var _this = this;
+      viewModeToggle.addEventListener('click', function() {
+        _this.eval();
+      });
+    },
 
-    viewModeToggle.on('click', function() {
-      _this.eval();
-    });
-  },
+    eval: function() {
+      var activeMode = document.getElementById('view-mode--toggle').checked;
 
-  eval: function() {
-    var activeMode = document.getElementById('view-mode--toggle').checked;
+      if ( activeMode === true ? this.darkMode() : this.lightMode() );
+    },
 
-    if ( activeMode === true ) {
-      this.darkMode();
-    } else {
-      this.lightMode();
-    }
-  },
+    darkMode: function() {
+      document.body.classList.add('view-mode--dark');
+    },
 
-  darkMode: function() {
-    $body.addClass('view-mode--dark');
-  },
+    lightMode: function() {
+      document.body.classList.remove('view-mode--dark');
+    },
+  }
 
-  lightMode: function() {
-    $body.removeClass('view-mode--dark');
-  },
-}
-
-viewMode.init();
+  viewMode.init();
+}());
