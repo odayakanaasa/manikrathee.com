fce641165b0cbdbe26bbc4a777de2f7ce6eb3efa
Manik Rathee <manikrathee@gmail.com>
2017-07-09 19:25:21 -0700

    production

--- _site/js/libs/animation-control.js
+++ _site/js/libs/animation-control.js
@@ -136,71 +136,73 @@ var vmPop5 = new mojs.Burst({
 // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-var viewModeAnimHook = document.getElementById('swoon');
-var viewModeAnimTimeline = new mojs.Timeline();
+// var swoonHook = document.getElementById('swoon');
+// var swoonTimeline = new mojs.Timeline();
 
-viewModeAnimHook.addEventListener('click', function(){
-    viewModeAnimTimeline.add(vmPop1, vmPop2, vmPop3, vmPop4, vmPop5).replay();
-});
+// if (swoonHook) {
+//   swoonHook.addEventListener('mouseover', function(){
+//       swoonTimeline.add(swoon1, swoon2, swoon3, swoon4).replay();
+//   });
 
-var vmPop1 = new mojs.Burst({
-    parent:       viewModeAnimHook,
-    count:        2,
-    radius:       {0:60},
-    children: {
-        fill :      '#988ADE',
-        duration:   1500,
-        easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
-        }
-    });
+//   var swoon1 = new mojs.Burst({
+//       parent:       swoonHook,
+//       count:        1,
+//       radius:       {0:60},
+//       children: {
+//           fill :      '#988ADE',
+//           duration:   1500,
+//           easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
+//           }
+//       });
 
-var vmPop2 = new mojs.Burst({
-    parent:   viewModeAnimHook,
-    count:    6,
-    radius:     {0:120},
-    children: {
-        fill:       '#DE8AA0',
-        duration:   1200,
-        delay:      50,
-        easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
-        }
-    });
 
-var vmPop3 = new mojs.Burst({
-    parent:       viewModeAnimHook,
-    count:        10,
-    radius:       {0:90},
-    children: {
-        fill:       '#8AAEDE',
-        duration:   1200,
-        delay:      100,
-        easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
-        }
-    });
+//   var swoon2 = new mojs.Burst({
+//       parent:       swoonHook,
+//       count:        30,
+//       radius:       {0:90},
+//       children: {
+//           fill:       '#DE8A8A',
+//           duration:   1200,
+//           delay:      100,
+//           easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
+//           }
+//       });
 
-var vmPop4 = new mojs.Burst({
-    parent: viewModeAnimHook,
-    count:    16,
-    radius:   {0:50},
-    children: {
-        fill:       '#8ADEAD',
-        duration:   1600,
-        delay:      175,
-        easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
-        }
-    });
+//   var swoon3 = new mojs.Burst({
+//       parent:   swoonHook,
+//       type: 'circle',
+//       fill: 'transparent',
+//       stroke: '#F35186',
+//       strokeWidth: {5:0},
+//       opacity: 0.4,
+//       count:    4,
+//       radius:     {0:230},
+//       x : -122,
+//       y : 33,
+//       easing: mojs.easing.sin.out,
+//       children: {
+//           fill:       '#DEC58A',
+//           duration:   1700,
+//           delay:      200,
+//           easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
+//           }
+//       });
 
-var vmPop5 = new mojs.Burst({
-    parent:   viewModeAnimHook,
-    count:    32,
-    radius:     {0:30},
-    children: {
-        fill:       '#DEC58A',
-        duration:   1700,
-        delay:      200,
-        easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
-        }
-    });
+//   var swoon4 = new mojs.Burst({
+//       parent:   swoonHook,
+//       type: 'circle',
+//       radius:     {0:2},
+//       x : 22,
+//       y : -44,
+//       easing: mojs.easing.sin.out,
+//       children: {
+//           fill:       '#DEC58A',
+//           duration:   2200,
+//           delay:      300,
+//           easing:     mojs.easing.bezier(0.19, 1, 0.22, 1)
+//           }
+//       });
+// }
 // ..........................................
 // ..........................................
 // ..........................................
